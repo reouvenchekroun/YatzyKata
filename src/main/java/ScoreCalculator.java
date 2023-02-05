ce715e0e20cbb2553fbb3b41e04af5c732b0609b
Reouven C <reouvenchekroun@live.fr>
2023-02-05 21:48:53 +0100

    removed static keyword and renaming Yatzy class into ScoreCalculator

--- src/main/java/ScoreCalculator.java
+++ src/main/java/ScoreCalculator.java
@@ -1,7 +1,7 @@
 import java.util.Comparator;
 import java.util.List;
 
-public class Yatzy {
+public class ScoreCalculator {
 
     public static final int YATZY_POINTS = 50;
     public static final int SMALL_STRAIGHT_POINTS = 15;
@@ -13,42 +13,39 @@ public class Yatzy {
     private static final List<DiceResult> LARGE_STRAIGHT_VALUES = List.of(
            DiceResult.TWO, DiceResult.THREE, DiceResult.FOUR, DiceResult.FIVE, DiceResult.SIX);
 
-    public static int chance(DiceWrapper diceWrapper)
-    {
+    public int chance(DiceWrapper diceWrapper) {
         return diceWrapper.sum();
     }
 
-    public static int yatzy(DiceWrapper diceWrapper)
-    {
+    public int yatzy(DiceWrapper diceWrapper) {
         return diceWrapper.hasAllDicesWithSameValue() ? YATZY_POINTS : NO_POINTS;
     }
 
-    public static int ones(DiceWrapper diceWrapper) {
+    public int ones(DiceWrapper diceWrapper) {
         return countPointsForDicesWithValue(diceWrapper, DiceResult.ONE);
     }
 
-    public static int twos(DiceWrapper diceWrapper) {
+    public int twos(DiceWrapper diceWrapper) {
         return countPointsForDicesWithValue(diceWrapper, DiceResult.TWO);
     }
 
-    public static int threes(DiceWrapper diceWrapper) {
+    public int threes(DiceWrapper diceWrapper) {
         return countPointsForDicesWithValue(diceWrapper, DiceResult.THREE);
     }
 
-    public static int fours(DiceWrapper diceWrapper) {
+    public int fours(DiceWrapper diceWrapper) {
         return countPointsForDicesWithValue(diceWrapper, DiceResult.FOUR);
     }
 
-    public static int fives(DiceWrapper diceWrapper) {
+    public int fives(DiceWrapper diceWrapper) {
         return countPointsForDicesWithValue(diceWrapper, DiceResult.FIVE);
     }
 
-    public static int sixes(DiceWrapper diceWrapper) {
+    public int sixes(DiceWrapper diceWrapper) {
         return countPointsForDicesWithValue(diceWrapper, DiceResult.SIX);
     }
 
-    public static int onePair(DiceWrapper diceWrapper)
-    {
+    public int onePair(DiceWrapper diceWrapper) {
         var highestPair = diceWrapper.getValuesWithFrequency(2).stream()
                 .max(Comparator.comparing(Integer::valueOf));
 
@@ -56,40 +53,35 @@ public class Yatzy {
                 .orElse(NO_POINTS);
     }
 
-    public static int twoPairs(DiceWrapper diceWrapper)
-    {
+    public int twoPairs(DiceWrapper diceWrapper) {
         return calculateXOfAKindScore(diceWrapper, 2);
     }
 
-    public static int threeOfAKind(DiceWrapper diceWrapper)
-    {
+    public int threeOfAKind(DiceWrapper diceWrapper) {
         return calculateXOfAKindScore(diceWrapper, 3);
     }
 
-    public static int fourOfAKind(DiceWrapper diceWrapper)
-    {
+    public int fourOfAKind(DiceWrapper diceWrapper) {
         return calculateXOfAKindScore(diceWrapper, 4);
     }
 
-    public static int smallStraight(DiceWrapper diceWrapper)
-    {
+    public int smallStraight(DiceWrapper diceWrapper) {
         return diceWrapper.containsResults(SMALL_STRAIGHT_VALUES) ? SMALL_STRAIGHT_POINTS : NO_POINTS;
     }
 
-    public static int largeStraight(DiceWrapper diceWrapper)
-    {
+    public int largeStraight(DiceWrapper diceWrapper) {
         return diceWrapper.containsResults(LARGE_STRAIGHT_VALUES) ? LARGE_STRAIGHT_POINTS : NO_POINTS;
     }
 
-    public static int fullHouse(DiceWrapper diceWrapper) {
+    public int fullHouse(DiceWrapper diceWrapper) {
         return diceWrapper.isFullHouse() ? diceWrapper.sum() : NO_POINTS;
     }
 
-    private static int countPointsForDicesWithValue(DiceWrapper diceWrapper, DiceResult diceResult) {
+    private int countPointsForDicesWithValue(DiceWrapper diceWrapper, DiceResult diceResult) {
         return diceWrapper.getOccurencesOfValue(diceResult) * diceResult.getValue();
     }
 
-    private static int calculateXOfAKindScore(DiceWrapper diceWrapper, Integer frequency) {
+    private int calculateXOfAKindScore(DiceWrapper diceWrapper, Integer frequency) {
         return diceWrapper.getValuesWithFrequency(frequency).stream()
                 .map(value -> value * frequency)
                 .mapToInt(Integer::valueOf)
